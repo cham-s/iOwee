5dc3d634737a5772c227255c022e02caa10d70e7
dash-c <deangreyhame@gmail.com>
2014-12-02 23:28:55 +0100

    test table view ok

--- Owee/TableViewCell.swift
+++ Owee/TableViewCell.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
     
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var statusLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak private var nameLabel: UILabel!
+    @IBOutlet weak private var statusLabel: UILabel!
+    @IBOutlet weak private var dateLabel: UILabel!
+    @IBOutlet weak private var priceLabel: UILabel!
 
+    // configure for the user of the class to only enter text directly - credits: NatashaTheRobots
+    func configure(#nameText: String, statusText: String, dateText:String,
+        priceText:String) {
+            nameLabel.text = nameText
+            nameLabel.accessibilityLabel = nameText
+            statusLabel.text = statusText
+            statusLabel.accessibilityLabel = statusText
+            dateLabel.text = dateText
+            dateLabel.accessibilityLabel = dateText
+            priceLabel.text = priceText
+            priceLabel.accessibilityLabel = "$ \(priceText)"
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- Owee/TableViewController.swift
+++ Owee/TableViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    //var owers =
+    private var owees = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var bill = Ower(firstName: "Billy", lastName: "Joe", email: "somemail@gmail.com",
+            phone: "111-111-111", address: "13 Street")
+        var pull = Item(ower: bill, object: "pull", value: "56", startDate: "12/12/12",
+            dueDate: "13/13/13", note: "carefull", image: UIImage(named: "sweat.png")!)
+        
+        for x in 0...6 {
+            owees.append(pull)
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -29,69 +38,21 @@ class TableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 10
+        return owees.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as TableViewCell
+        
+        let owee = owees[indexPath.row]
         // Configure the cell...
-
+        cell.configure(nameText: owee.ower.firstName, statusText: owee.status, dateText: owee.startDate, priceText: owee.value)
+        
+        
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
